85963baea81483ee6c15d8a6152598958868bee1
Алексей Редькин <a.redkin@balance-pl.ru>
2024-02-11 17:41:21 +0300

    lesson1 simple calculator

--- lib/pet_project.ex
+++ lib/pet_project.ex
@@ -1,18 +1,56 @@
 defmodule PetProject do
   @moduledoc """
-  Documentation for `PetProject`.
+  Base module for `PetProject`.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PetProject.hello()
-      :world
-
+  The Calculator itself
   """
-  def hello do
-    :world
+  def perform do
+    operation =
+      "What operation would you like to do: +, -, /, *, **?\n"
+      |> input_helper
+      |> validate_operator
+
+    attr1 =
+      "Enter a integer or irrational number as a first argument\n"
+      |> input_helper
+      |> parse_attr
+
+    attr2 =
+      "Enter a integer or irrational number as a second argument\n"
+      |> input_helper
+      |> parse_attr
+
+    case operation do
+      "+" -> Actions.sum(attr1, attr2)
+      "-" -> Actions.subtract(attr1, attr2)
+      "/" -> Actions.division(attr1, attr2)
+      "*" -> Actions.multiplication(attr1, attr2)
+      "**" -> Actions.power(attr1, attr2)
+    end
+  end
+
+  def input_helper(string) do
+    string
+    |> IO.gets()
+    |> String.trim()
+  end
+
+  def validate_operator(operator) when operator in ["+", "-", "*", "**", "/"] do
+    operator
+  end
+
+  def validate_operator(_operator) do
+    raise "Invalid operator, please try again"
+  end
+
+  def parse_attr(attr) do
+    attr
+    |> String.contains?(".")
+    |> case do
+      true -> String.to_float(attr)
+      false -> String.to_integer(attr)
+    end
   end
 end
