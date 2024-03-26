e36c5b06340ba3cfa0fe2b25856a377c9662ed6a
Алексей Редькин <a.redkin@balance-pl.ru>
2024-03-26 22:52:41 +0300

    Имплементация протокола Lister для базовых типов

---
+++ lib/protocols/lister.ex
@@ -0,0 +1,35 @@
+defprotocol Lister do
+  def to_list(data)
+end
+
+defimpl Lister, for: List do
+  def to_list(list), do: list
+end
+
+defimpl Lister, for: Tuple do
+  defdelegate to_list(tuple), to: Tuple
+end
+
+defimpl Lister, for: Map do
+  defdelegate to_list(map), to: Enum
+end
+
+defimpl Lister, for: BitString do
+  def to_list(string), do: String.split(string)
+end
+
+defimpl Lister, for: Atom do
+  def to_list(atom), do: Atom.to_string(atom) |> String.split()
+end
+
+defimpl Lister, for: Integer do
+  def to_list(integer), do: Integer.digits(integer)
+end
+
+defimpl Lister, for: Float do
+  def to_list(float), do: Float.to_string(float) |> String.split()
+end
+
+defimpl Lister, for: Date do
+  def to_list(date), do: Date.to_string(date) |> String.split()
+end
