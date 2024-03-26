acb819eb2a1ffd90c9d8b55b8f26f2c9e97140f4
Алексей Редькин <a.redkin@balance-pl.ru>
2024-03-26 23:16:36 +0300

    тесты

--- lib/protocols/lister.ex
+++ lib/protocols/lister.ex
@@ -15,11 +15,11 @@ defimpl Lister, for: Map do
 end
 
 defimpl Lister, for: BitString do
-  def to_list(string), do: String.split(string)
+  def to_list(string), do: String.split(string, "", trim: true)
 end
 
 defimpl Lister, for: Atom do
-  def to_list(atom), do: Atom.to_string(atom) |> String.split()
+  def to_list(atom), do: Atom.to_string(atom) |> String.split("", trim: true)
 end
 
 defimpl Lister, for: Integer do
@@ -27,9 +27,9 @@ defimpl Lister, for: Integer do
 end
 
 defimpl Lister, for: Float do
-  def to_list(float), do: Float.to_string(float) |> String.split()
+  def to_list(float), do: Float.to_string(float) |> String.split("", trim: true)
 end
 
 defimpl Lister, for: Date do
-  def to_list(date), do: Date.to_string(date) |> String.split()
+  def to_list(date), do: Date.to_string(date) |> String.split("", trim: true)
 end
