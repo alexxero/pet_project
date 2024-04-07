259340d0c75ce83ee7c37715efa20c5a038d282c
Алексей Редькин <a.redkin@balance-pl.ru>
2024-04-07 17:54:22 +0300

    Text-generation model client

---
+++ lib/pet_project/application.ex
@@ -0,0 +1,17 @@
+defmodule PetProject.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [PetProject.TextGenerator]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: PetProject.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
